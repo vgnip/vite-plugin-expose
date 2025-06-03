6164073801cd3d3af00d9485ed4146ca890d2ef9
dengping <ping.a.deng@cisdi.com.cn>
2025-06-03 20:39:14 +0800

    feat: 新增类型声明

---
+++ lib/index.d.ts
@@ -0,0 +1,8 @@
+export declare const vitePluginExpose: (options?: any) => {
+    name: string;
+    transform(code: string, flePath: string): {
+        code: string;
+        map: null;
+    } | null;
+};
+//# sourceMappingURL=index.d.ts.map
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 import path from 'path';
 import { fileURLToPath } from "url";
+import dts from 'vite-plugin-dts'
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 export default defineConfig({
@@ -12,4 +13,15 @@ export default defineConfig({
             formats: ['es']
         }
     },
+    plugins: [
+        dts({
+            insertTypesEntry: true, // 生成类型声明入口
+            include: ['src'],      // 包含的ts文件
+            exclude: ['**/__tests__/**'], // 排除测试文件
+            beforeWriteFile: (filePath, content) => {
+                // 可选：自定义类型文件写入逻辑
+                return { filePath, content }
+            }
+        })
+    ]
 });
